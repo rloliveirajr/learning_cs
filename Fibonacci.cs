7651dbf0b28b2e678e756bd83b61fcd28a9f477d
U-NORTHAMERICA\robeoliv <robeoliv@RdeOliveira-LT.northamerica.corp.microsoft.com>
2015-12-29 18:07:53 -0800

    First commit. Add generic fibonacci calculation implemented with reflection.

---
+++ Fibonacci.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace BeginningCS
+{
+    class Fibonacci
+    {
+        public static long Recursive(long n)
+        {
+            if(n < 2)
+            {
+                return n;
+            }
+            
+            return Fibonacci.Recursive(n - 1) + Fibonacci.Recursive(n - 2);
+        }
+
+        public static long RecursiveTernary(long n)
+        {
+            return n < 2 ? n : Fibonacci.RecursiveTernary(n - 1) + Fibonacci.RecursiveTernary(n - 2);
+        }
+
+        public static long TailRecursive(long n)
+        {
+            return Fibonacci.TailRecursive(n, 0, 1);
+        }
+        private static long TailRecursive(long n, long a, long b)
+        {
+            if(n < 1)
+            {
+                return a;
+            }
+
+            return Fibonacci.TailRecursive(n - 1, a + b, a);
+        }
+
+        public static long BinetsFormula(long n)
+        {
+            double fi = (1 + Math.Sqrt(5)) / 2;
+
+            return (long) ((Math.Pow(fi, n) - Math.Pow(1 - fi, n)) / Math.Sqrt(5));
+        }
+
+        public static long Iterative(long n)
+        {
+            long i = 0;
+            long j = 1;
+            long k, t;
+
+            for(k = 1; k < n; k++)
+            {
+                t = i + j;
+                i = j;
+                j = t;
+            }
+
+            return j;
+        }
+
+        public static long ExponetiationBySquaring(long n)
+        {
+            long i = n - 1;
+            long a = 1;
+            long b = 0;
+            long c = 0;
+            long d = 1;
+            long t;
+
+            if (n <= 0) 
+                return 0;
+
+            while(i > 0)
+            {
+                if (i % 2 == 1)
+                {
+                    t = d * (b + a) + c * b;
+                    a = d * b + c * a;
+                    b = t;
+                }
+                t = d * (2 * c + d);
+                c = c * c + d * d;
+                d = t;
+                i = i / 2;
+            }
+
+            return a + b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace BeginningCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 50;
+            string computation = "BeginningCS.Fibonacci";
+            var methodEnumerable = RetrieveMethods(computation);
+
+            foreach (MethodInfo methodInfo in methodEnumerable)
+            {
+                run(methodInfo, n);
+            }
+
+        }
+
+        private static IEnumerable<MethodInfo> RetrieveMethods(string computation)
+        {
+            Type a = Type.GetType(computation);
+            MethodInfo[] methods = a.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            IEnumerable<MethodInfo> methodEnumarable = methods.OrderBy(method => method.Name);
+            
+            return methodEnumarable;
+        }
+
+        static void run(MethodInfo methodInfo, long n)
+        {
+            Func<long, long>  method = (Func<long, long>)Delegate.
+                CreateDelegate(typeof(Func<long, long>), methodInfo);
+                
+            string output_format = "Fibonacci of " + n + " is {0} ({1})";
+            
+            Stopwatch watch = Stopwatch.StartNew();
+            
+            long result = method(n); watch.Stop();
+
+            string text = String.Format(output_format, result, methodInfo.Name);
+            Console.WriteLine(text + ": Took " + watch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
