7785c3c227e70aa65a4cc43b23cda7e997182778
U-NORTHAMERICA\robeoliv <robeoliv@MININT-MVQIKJ1.northamerica.corp.microsoft.com>
2015-12-31 12:55:57 -0800

    Implements QuickSort

---
+++ Sorting/QuickSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginningCS.Sorting
+{
+    class QuickSort
+    {
+        public static void DoIt(int[] list)
+        {
+            Partition(list, 0, list.Length-1);
+        }
+
+        private static void Partition(int[] list, int lo, int hi)
+        {
+            if (lo < hi)
+            {
+                int p = Sort(list, lo, hi);
+                Partition(list, lo, p - 1);
+                Partition(list, p + 1, hi);
+            }
+        }
+
+        private static int Sort(int[] list, int lo, int hi)
+        {
+            int pivot = list[hi];
+            int i = lo;
+            for (int j = lo; j < hi; j++)
+            {
+                if (list[j] <= pivot)
+                {
+                    SortUtils.Swap(list, i, j);
+                    i++;
+                }
+            }
+            SortUtils.Swap(list, i, hi);
+            return i;
+        }
+    }
+}
--- Sorting/SortMethods.cs
+++ Sorting/SortMethods.cs
@@ -11,8 +11,8 @@ namespace BeginningCS.Sorting
     {
         public static void Run()
         {
-            int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9 };
-            MergeTopDown.DoIt(a);
+            int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -4, -3, -5, -6, -7, -8, -9 };
+            QuickSort.DoIt(a);
             Console.WriteLine(string.Join(",", a));
         }
     }
