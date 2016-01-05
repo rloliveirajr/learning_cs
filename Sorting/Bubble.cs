03811f018d36be20b76966be0bc93a2a4b53efb6
U-NORTHAMERICA\robeoliv <robeoliv@MININT-MVQIKJ1.northamerica.corp.microsoft.com>
2016-01-04 16:40:20 -0800

    Small changes

---
+++ Sorting/Bubble.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginningCS.Sorting
+{
+    class Bubble
+    {
+        public static void DoIt(int[] list)
+        {
+            for (int i = 1; i < list.Length; i++)
+            {
+                for (int j = 0; j < list.Length; j++)
+                {
+                    if (list[i] < list[j])
+                    {
+                        SortUtils.Swap(list, i, j);
+                    }
+                }
+            }
+        }
+    }
+}
--- Sorting/SortMethods.cs
+++ Sorting/SortMethods.cs
@@ -12,7 +12,7 @@ namespace BeginningCS.Sorting
         public static void Run()
         {
             int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -4, -3, -5, -6, -7, -8, -9 };
-            QuickSort.DoIt(a);
+            Bubble.DoIt(a);
             Console.WriteLine(string.Join(",", a));
         }
     }
